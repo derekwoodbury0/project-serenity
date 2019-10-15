582abb692375b7db13e71126848ec7731ab2c564
Derek <Derek@Dereks-MacBook-Pro.local>
2019-10-15 00:40:07 -0600

    initial commit, nav added

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Nav.css'
+
+export default function Nav() {
+    return (
+    <div className="nav-page-main">
+        <div className="nav-page-links">
+            <h3>Home</h3>
+            <h3>About</h3>
+            <h3>Mission</h3>
+            <h3>Contact</h3>
+        </div>
+    </div>
+    )
+}
